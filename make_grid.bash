5461095e8e477592a0626d113816ffd8bfab2381
Kovas Palunas <kovas.palunas@gmail.com>
2021-01-04 16:48:50 -0800

    Made card grids bigger to avoid splitting into multiple sheets.

--- make_grid.bash
+++ make_grid.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 shopt -s extglob
-montage -tile 10x7 -geometry +0+0 -border 0 resource_+([0-9]).png resource_deck.png
-montage -tile 10x7 -geometry +0+0 -border 0 improvement_+([0-9]).png improvement_deck.png
-montage -tile 10x7 -geometry +0+0 -border 0 people_+([0-9]).png people_deck.png
+montage -tile 20x20 -geometry +0+0 -border 0 resource_+([0-9]).png resource_deck.png
+montage -tile 20x20 -geometry +0+0 -border 0 improvement_+([0-9]).png improvement_deck.png
+montage -tile 20x20 -geometry +0+0 -border 0 people_+([0-9]).png people_deck.png
